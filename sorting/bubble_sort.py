03790026e639e142accd4af378795df098bb6fa4
pbhagyashri <bhagyashri.padmawar@sideinc.com>
2025-03-01 20:19:48 -0500

    add comments

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,14 +1,21 @@
+# brut force algorithm with quadratic O(n**2) time complexity
 def bubble_sort(arr):
+  # start the outer for loop from right to left decresing the index by 1
   for i in range(len(arr) - 1, 0, -1):
-    
+    # start another inner for loop from index i to 0. (i + 1 ensures we don't run into out of bound index)
     for j in range(i + 1, 0, -1):
+      # create 2 pointers, 
+      # right pointer, r points to right most element at index i arr[i]
+      # left pointer, l points to the element left to right most element, j - 1
       r = i
-      l = j - 1
+      l = j - 1 # (j - 1 ensures we don't run into out of bound index)
 
+      # if element on the right is smaller than the element on left, swap the 2 elements
+      # bubbling up the smallest element to start of the array
       if(arr[r] < arr[l]):
         arr[l], arr[r] = arr[r], arr[l]
 
-arr = [1,9,2,0,3,7, 1]
+arr = [1, 9, 2, 0, 3, 7, 1]
 bubble_sort(arr)
 
 print('arr', arr)
--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,31 +1,21 @@
-# def selection_sort(arr):
-#   if len(arr) <= 1:
-#     return arr
-  
-#   for i in range(0, len(arr)):
-#     curr_min_index = i
-#     temp = arr[i]
-#     for j in range(i + 1, len(arr) - 1):
-#       if arr[j] < arr[curr_min_index]:
-#         curr_min_index = j
-
-#     arr[i] = arr[curr_min_index]
-#     arr[curr_min_index] = temp
-
+# brut force algorithm with quadratic O(n**2) time complexity
 def selection_sort(arr):
     if len(arr) <= 1:
         return arr
         
     for i in range(0, len(arr)):
+        # record current minimum
         curr_min_index = i
-        temp = arr[i]
         
+        # start the second iteration from i+1 to length of the array
         for j in range(i + 1, len(arr)):
+            # see if any of the numbers in the array starting from index i+1 is smaller that current minimun at index i
             if(arr[j] < arr[curr_min_index]):
+                # if new minimum is found, replace it's index with current minimum index
                 curr_min_index = j
-
-        arr[i] = arr[curr_min_index]
-        arr[curr_min_index] = temp
+                
+        # swap the current minimum with new minimum
+        arr[i], arr[curr_min_index] = arr[curr_min_index], arr[i]
         
     return arr
 
